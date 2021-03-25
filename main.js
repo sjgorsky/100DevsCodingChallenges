50ded44f51480e7cf1d6fd440a8797e723a8ed14
sjgorsky <sgorsky@gmail.com>
2021-03-24 20:28:25 -0500

    Add files via upload
    object creation practice

--- main.js
+++ main.js
@@ -1,24 +1,10 @@
-//Arrays
+//Create a stopwatch object that has four properties and three methods
+let stopwatch = {}
+    stopwatch.color='black'
+    stopwatch.brand='tag heuer'
+    stopwatch.startTime = 0.00
+    stopwatch.maxTime=99.99
 
-//Create and array of tv shows. Loop through and print each show to the console
-let tvShows = ['Knight Rider', 'JTV', 'Frasier', 'Mad Men']
-    tvShows.forEach((x,i) => (console.log(x)))
-
-
-//Create and array of numbers
-//Return a new array of numbers that includes every even number from the previous Arrays
-let numArr = [3,4,5,6,7,8]
-function getEven(arr){
-    return arr.filter(x => x % 2 == 0)
-}
-
-console.log(getEven(numArr))
-
-
-//Create a function that takes in an array of numbers
-//Alert the sum of the second lowest and the second highest number 
-function secondTake(arr){
-    let sorted = arr.sort((a,b)=>a-b)
-    alert(sorted[1] + sorted[sorted.length-2])
-}
-secondTake([4,5,6,7,8])
+    stopwatch.runTime= function(){console.log('Running!')}
+    stopwatch.stopTime=function(){console.log('Stopping!')}
+    stopwatch.resetTime=function(){console.log('Reset!')}
