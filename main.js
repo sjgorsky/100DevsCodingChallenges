63d647b8621abe6475d81f26694a22eacb913059
sjgorsky <sgorsky@gmail.com>
2021-03-29 20:50:24 -0500

    string reversing practice && palindrome checker

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-//Create a stopwatch object that has four properties and three methods
-let stopwatch = {}
-    stopwatch.color='black'
-    stopwatch.brand='tag heuer'
-    stopwatch.startTime = 0.00
-    stopwatch.maxTime=99.99
+//Arrays
 
-    stopwatch.runTime= function(){console.log('Running!')}
-    stopwatch.stopTime=function(){console.log('Stopping!')}
-    stopwatch.resetTime=function(){console.log('Reset!')}
+//Create and array of numbers. Sum all of the numbers. Alert the sum.
+let arr = [1,2,3,4,45]
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+console.log(arr.reduce(reducer))
+//Create a function that takes in an array of numbers
+//Return a new array of numbers that is every original number squared
+function squareOriginal(arr){
+    let newArr = arr.map(x => x**2)
+    console.log(newArr)
+}
+squareOriginal([3,4,5])
+//Create a function that takes string
+//Print the reverse of that string to the console
+function stringReverse(str){
+    console.log(str.split('').reverse().join(''))
+}
+stringReverse('Why hello there!')
+//Create a function that takes in a string
+//Alert if the string is a palindrome or not
+function palindromeCheck(str){
+    let strReverser=str.split('').reverse().join('')
+    if (str == strReverser){
+        alert('That\'s a palindrome!')
+    }
+}
+palindromeCheck('not')
